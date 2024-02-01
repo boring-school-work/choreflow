39e72e44521a73ac22f797259bf35a0f94168273
David Saah <dasorange.hope@gmail.com>
2024-02-01 14:15:04 +0000

    refactor: conform to folder structure given

---
+++ db/chores_mgt.sql
@@ -0,0 +1,277 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 29, 2024 at 09:22 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chores_mgt`
+--
+
+-- --------------------------------------------------------
+
+-- Table structure for table `Assigned_people`#
+
+
+--
+
+
+Drop database if exists chores_mgt;
+
+CREATE database chores_mgt;
+
+use chores_mgt;
+
+
+CREATE TABLE `Assigned_people` (
+  `pid` int(11) NOT NULL,
+  `assignmentid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Assignment`
+--
+
+CREATE TABLE `Assignment` (
+  `assignmentid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `date_assign` date NOT NULL,
+  `date_due` date NOT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `date_completed` date NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `who_assigned` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Chores`
+--
+
+CREATE TABLE `Chores` (
+  `cid` int(11) NOT NULL,
+  `chorname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Family_name`
+--
+
+CREATE TABLE `Family_name` (
+  `fid` int(11) NOT NULL,
+  `fam_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Family_name`
+--
+
+INSERT INTO `Family_name` (`fid`, `fam_name`) VALUES
+(1, 'Father'),
+(2, 'Mother'),
+(3, 'Son'),
+(4, 'Daughter'),
+(5, 'Sister'),
+(6, 'Brother');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `People`
+--
+
+CREATE TABLE `People` (
+  `pid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
+  `fid` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `dob` date NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `passwd` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Role`
+--
+
+CREATE TABLE `Role` (
+  `rid` int(11) NOT NULL,
+  `rname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Role`
+--
+
+INSERT INTO `Role` (`rid`, `rname`) VALUES
+(1, 'superadmin'),
+(2, 'admin'),
+(3, 'standard');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Status`
+--
+
+CREATE TABLE `Status` (
+  `sid` int(11) NOT NULL,
+  `sname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Status`
+--
+
+INSERT INTO `Status` (`sid`, `sname`) VALUES
+(1, 'Assigned'),
+(2, 'InProgress'),
+(3, 'Completed'),
+(4, 'Incomplete');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Assigned_people`
+--
+ALTER TABLE `Assigned_people`
+  ADD KEY `assignmentid` (`assignmentid`),
+  ADD KEY `pid` (`pid`);
+
+--
+-- Indexes for table `Assignment`
+--
+ALTER TABLE `Assignment`
+  ADD PRIMARY KEY (`assignmentid`),
+  ADD KEY `cid` (`cid`),
+  ADD KEY `sid` (`sid`),
+  ADD KEY `who_assigned` (`who_assigned`);
+
+--
+-- Indexes for table `Chores`
+--
+ALTER TABLE `Chores`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `Family_name`
+--
+ALTER TABLE `Family_name`
+  ADD PRIMARY KEY (`fid`);
+
+--
+-- Indexes for table `People`
+--
+ALTER TABLE `People`
+  ADD PRIMARY KEY (`pid`),
+  ADD KEY `rid` (`rid`),
+  ADD KEY `fid` (`fid`);
+
+--
+-- Indexes for table `Role`
+--
+ALTER TABLE `Role`
+  ADD PRIMARY KEY (`rid`);
+
+--
+-- Indexes for table `Status`
+--
+ALTER TABLE `Status`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Assignment`
+--
+ALTER TABLE `Assignment`
+  MODIFY `assignmentid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Chores`
+--
+ALTER TABLE `Chores`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Family_name`
+--
+ALTER TABLE `Family_name`
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `People`
+--
+ALTER TABLE `People`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Role`
+--
+ALTER TABLE `Role`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Status`
+--
+ALTER TABLE `Status`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Assigned_people`
+--
+ALTER TABLE `Assigned_people`
+  ADD CONSTRAINT `assigned_people_ibfk_1` FOREIGN KEY (`assignmentid`) REFERENCES `Assignment` (`assignmentid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `assigned_people_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `People` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Assignment`
+--
+ALTER TABLE `Assignment`
+  ADD CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `Chores` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `assignment_ibfk_2` FOREIGN KEY (`sid`) REFERENCES `Status` (`sid`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `assignment_ibfk_3` FOREIGN KEY (`who_assigned`) REFERENCES `People` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `People`
+--
+ALTER TABLE `People`
+  ADD CONSTRAINT `people_ibfk_1` FOREIGN KEY (`rid`) REFERENCES `Role` (`rid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `people_ibfk_2` FOREIGN KEY (`fid`) REFERENCES `Family_name` (`fid`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -1,43 +1,38 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Chore Management System</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="./assets/css/styles.css" rel="stylesheet" />
-    <link href="./assets/css/output.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="py-6 px-6 border-b-4 border-blue-500 rounded">
-      <h1 class="font-semibold text-4xl text-left">ChoreFlow</h1>
+
+<head>
+  <title>Chore Management System</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./assets/css/styles.css" rel="stylesheet" />
+  <link href="./assets/css/output.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="py-6 px-6 border-b-4 border-blue-500 rounded">
+    <h1 class="font-semibold text-4xl text-left">ChoreFlow</h1>
+  </div>
+  <div class="bg-gray-100 h-screen text-center flex flex-col pt-20">
+    <h3 class="text-2xl font-semibold mb-2">Welcome to ChoreFlow</h3>
+    <div class="text-gray-600 mb-4">
+      <p>
+        The site is currently under construction. Pick a role to explore the
+        website.
+      </p>
+      <p>Thank you!</p>
     </div>
-    <div class="bg-gray-100 h-screen text-center flex flex-col pt-20">
-      <h3 class="text-2xl font-semibold mb-2">Welcome to ChoreFlow</h3>
-      <div class="text-gray-600 mb-4">
-        <p>
-          The site is currently under construction. Pick a role to explore the
-          website.
-        </p>
-        <p>Thank you!</p>
-      </div>
-      <div>
-        <a href="./admin/dashboard/">
-          <button
-            type="button"
-            class="bg-red-400 px-4 py-1.5 text-white font-semibold rounded-lg mr-3"
-          >
-            Admin
-          </button></a
-        >
-        <a href="./user/dashboard/">
-          <button
-            type="button"
-            class="bg-blue-400 px-4 py-1.5 text-white font-semibold rounded-lg"
-          >
-            User
-          </button></a
-        >
-      </div>
+    <div>
+      <a href="./view/admin/dashboard/">
+        <button type="button" class="bg-red-400 px-4 py-1.5 text-white font-semibold rounded-lg mr-3">
+          Admin
+        </button></a>
+      <a href="./view/user/dashboard/">
+        <button type="button" class="bg-blue-400 px-4 py-1.5 text-white font-semibold rounded-lg">
+          User
+        </button></a>
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
