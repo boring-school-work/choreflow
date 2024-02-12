0b0716e74e71ecd113679004ee6983fa7f19a560
David Saah <dasorange.hope@gmail.com>
2024-02-12 12:32:01 +0000

    chore: comment out tests

--- settings/connection.php
+++ settings/connection.php
@@ -14,5 +14,5 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-echo $conn->host_info . "<br>";
-echo "Database connected successfully <br>";
+/* echo $conn->host_info . "<br>"; */
+/* echo "Database connected successfully <br>"; */
