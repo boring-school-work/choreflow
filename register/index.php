181f315e3ebc1b7f1d3101792141243e785abe72
David Saah <dasorange.hope@gmail.com>
2024-02-12 12:32:52 +0000

    feat: pull user roles from database

--- register/index.php
+++ register/index.php
@@ -1,151 +1,99 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Chore Management System</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="./../assets/css/styles.css" rel="stylesheet" />
-    <link href="./../assets/css/output.css" rel="stylesheet" />
-  </head>
-  <body>
-    <!-- Dashboard -->
-    <div class="py-6 px-6 border-b-4 border-blue-500 rounded">
-      <a href="/" class="font-semibold text-4xl text-left">ChoreFlow</a>
-    </div>
-    <div class="bg-gray-100 h-screen text-center flex flex-col pt-20">
-      <h2 class="font-semibold text-3xl mb-5">Register your account</h2>
-      <form action="" method="post" name="register" id="register">
-        <div class="flex flex-row place-content-center gap-x-3 mb-4">
-          <div class="flex flex-col text-left mb-1 w-1/5">
-            <label for="fname">First name</label
-            ><input
-              type="text"
-              name="fname"
-              id="fname"
-              class="py-2 px-3 rounded-md"
-              placeholder="Enter your first name"
-              required
-            />
-          </div>
-          <div class="flex flex-col text-left mb-1 w-1/5">
-            <label for="fname">Last name</label
-            ><input
-              type="text"
-              name="lname"
-              id="lname"
-              class="py-2 px-3 rounded-md"
-              placeholder="Enter your last name"
-              required
-            />
-          </div>
+
+<head>
+  <title>Chore Management System</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./../assets/css/styles.css" rel="stylesheet" />
+  <link href="./../assets/css/output.css" rel="stylesheet" />
+</head>
+
+<body>
+  <!-- Dashboard -->
+  <div class="py-6 px-6 border-b-4 border-blue-500 rounded">
+    <a href="/" class="font-semibold text-4xl text-left">ChoreFlow</a>
+  </div>
+  <div class="bg-gray-100 h-screen text-center flex flex-col pt-20">
+    <h2 class="font-semibold text-3xl mb-5">Register your account</h2>
+    <form action="" method="post" name="register" id="register">
+      <div class="flex flex-row place-content-center gap-x-3 mb-4">
+        <div class="flex flex-col text-left mb-1 w-1/5">
+          <label for="fname">First name</label><input type="text" name="fname" id="fname" class="py-2 px-3 rounded-md" placeholder="Enter your first name" required />
         </div>
-        <div class="flex flex-row place-content-center gap-x-3 mb-4">
-          <div class="flex flex-col text-left mb-1 w-1/5">
-            <p>Gender</p>
-            <div>
-              <div class="indent-5">
-                <input type="radio" id="male" name="gender" value="male" />
-                <label for="male">male</label><br />
-              </div>
-              <div class="indent-5">
-                <input type="radio" id="female" name="gender" value="female" />
-                <label for="female">female</label><br />
-              </div>
+        <div class="flex flex-col text-left mb-1 w-1/5">
+          <label for="fname">Last name</label><input type="text" name="lname" id="lname" class="py-2 px-3 rounded-md" placeholder="Enter your last name" required />
+        </div>
+      </div>
+      <div class="flex flex-row place-content-center gap-x-3 mb-4">
+        <div class="flex flex-col text-left mb-1 w-1/5">
+          <p>Gender</p>
+          <div>
+            <div class="indent-5">
+              <input type="radio" id="male" name="gender" value="male" />
+              <label for="male">male</label><br />
+            </div>
+            <div class="indent-5">
+              <input type="radio" id="female" name="gender" value="female" />
+              <label for="female">female</label><br />
             </div>
           </div>
-          <div class="flex flex-col text-left mb-1 w-1/5">
-            <p>Family role</p>
-            <select
-              name="family_role"
-              class="w-64 py-1.5 px-1 rounded-md bg-white"
-            >
-              <option value="">Pick a family role</option>
-              <option value="father">Father</option>
-              <option value="mother">Mother</option>
-              <option value="son">Son</option>
-              <option value="daughter">Daughter</option>
-            </select>
-          </div>
         </div>
-        <div class="flex flex-row place-content-center gap-x-3 mb-4">
-          <div class="flex flex-col text-left mb-1 w-1/5">
-            <label for="dob">Date of birth</label
-            ><input
-              type="date"
-              name="dob"
-              id="dob"
-              class="py-2 px-3 rounded-md"
-              required
-            />
-          </div>
-          <div class="flex flex-col text-left mb-1 w-1/5">
-            <label for="tel">Telephone</label
-            ><input
-              type="tel"
-              name="tel"
-              id="tel"
-              class="py-2 px-3 rounded-md"
-              placeholder="Enter your phone number"
-            />
-          </div>
+        <div class="flex flex-col text-left mb-1 w-1/5">
+          <p>Family role</p>
+          <select name="family_role" class="w-64 py-1.5 px-1 rounded-md bg-white">
+            <option value="">Pick a family role</option>
+            <?php
+            include '../functions/select_role.php';
+
+            foreach ($result as $row) {
+              echo "<option value='" . $row['fid'] . "'>" . $row['fam_name'] . "</option>";
+            }
+            ?>
+          </select>
         </div>
-        <div class="grid grid-cols-2 place-content-center gap-x-3 mb-4">
-          <div class="place-self-end self-center">
-            <label for="email">Enter your email</label>
-          </div>
-          <div class="w-[40%]">
-            <input
-              class="w-full py-2 px-3 rounded-md"
-              type="text"
-              name="email"
-              placeholder="Email"
-            />
-          </div>
-        </div>
-        <div class="grid grid-cols-2 place-content-center gap-x-3 mb-4">
-          <div class="place-self-end self-center">
-            <label for="pass1">Enter your password</label>
-          </div>
-          <div class="w-[40%]">
-            <input
-              class="w-full py-2 px-3 rounded-md"
-              type="password"
-              name="pass1"
-              id="pass1"
-              placeholder="Password"
-              required
-            />
-          </div>
-        </div>
-        <div class="grid grid-cols-2 place-content-center gap-x-3 mb-4">
-          <div class="place-self-end self-center">
-            <label for="pass2">Re-type your password</label>
-          </div>
-          <div class="w-[40%]">
-            <input
-              class="w-full py-2 px-3 rounded-md"
-              type="password"
-              name="pass2"
-              id="pass2"
-              placeholder="Re-type password"
-              required
-            />
-          </div>
-        </div>
-        <div class="my-3">
-          <input
-            class="py-2 bg-blue-400 px-7 rounded-md"
-            type="submit"
-            name="submit"
-            id="submit"
-            value="Register"
-          />
-        </div>
-      </form>
-      <div>
-        Already have an account?
-        <a class="text-blue-800" href="./../login">Sign in</a>.
       </div>
+      <div class="flex flex-row place-content-center gap-x-3 mb-4">
+        <div class="flex flex-col text-left mb-1 w-1/5">
+          <label for="dob">Date of birth</label><input type="date" name="dob" id="dob" class="py-2 px-3 rounded-md" required />
+        </div>
+        <div class="flex flex-col text-left mb-1 w-1/5">
+          <label for="tel">Telephone</label><input type="tel" name="tel" id="tel" class="py-2 px-3 rounded-md" placeholder="Enter your phone number" />
+        </div>
+      </div>
+      <div class="grid grid-cols-2 place-content-center gap-x-3 mb-4">
+        <div class="place-self-end self-center">
+          <label for="email">Enter your email</label>
+        </div>
+        <div class="w-[40%]">
+          <input class="w-full py-2 px-3 rounded-md" type="text" name="email" placeholder="Email" />
+        </div>
+      </div>
+      <div class="grid grid-cols-2 place-content-center gap-x-3 mb-4">
+        <div class="place-self-end self-center">
+          <label for="pass1">Enter your password</label>
+        </div>
+        <div class="w-[40%]">
+          <input class="w-full py-2 px-3 rounded-md" type="password" name="pass1" id="pass1" placeholder="Password" required />
+        </div>
+      </div>
+      <div class="grid grid-cols-2 place-content-center gap-x-3 mb-4">
+        <div class="place-self-end self-center">
+          <label for="pass2">Re-type your password</label>
+        </div>
+        <div class="w-[40%]">
+          <input class="w-full py-2 px-3 rounded-md" type="password" name="pass2" id="pass2" placeholder="Re-type password" required />
+        </div>
+      </div>
+      <div class="my-3">
+        <input class="py-2 bg-blue-400 px-7 rounded-md" type="submit" name="submit" id="submit" value="Register" />
+      </div>
+    </form>
+    <div>
+      Already have an account?
+      <a class="text-blue-800" href="./../login">Sign in</a>.
     </div>
-  </body>
+  </div>
+</body>
+
 </html>
