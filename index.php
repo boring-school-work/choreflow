690ef341d4b7aff57314c56a0c60af29ec8aea5b
David Saah <dasorange.hope@gmail.com>
2024-02-16 11:28:51 +0000

    feat: redirect to login when root url is visited

--- index.php
+++ index.php
@@ -1,38 +1,9 @@
-<!doctype html>
-<html lang="en">
+<!-- redirect to login-->
+<?php
+if (isset($_SESSION['user_id'])) {
+  header("Location: ./view/dashboard/");
+}
 
-<head>
-  <title>Chore Management System</title>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="./assets/css/styles.css" rel="stylesheet" />
-  <link href="./assets/css/output.css" rel="stylesheet" />
-</head>
-
-<body>
-  <div class="py-6 px-6 border-b-4 border-blue-500 rounded">
-    <h1 class="font-semibold text-4xl text-left">ChoreFlow</h1>
-  </div>
-  <div class="bg-gray-100 h-screen text-center flex flex-col pt-20">
-    <h3 class="text-2xl font-semibold mb-2">Welcome to ChoreFlow</h3>
-    <div class="text-gray-600 mb-4">
-      <p>
-        The site is currently under construction. Pick a role to explore the
-        website.
-      </p>
-      <p>Thank you!</p>
-    </div>
-    <div>
-      <a href="./view/admin/dashboard/">
-        <button type="button" class="bg-red-400 px-4 py-1.5 text-white font-semibold rounded-lg mr-3">
-          Admin
-        </button></a>
-      <a href="./view/user/dashboard/">
-        <button type="button" class="bg-blue-400 px-4 py-1.5 text-white font-semibold rounded-lg">
-          User
-        </button></a>
-    </div>
-  </div>
-</body>
-
-</html>
+header('Location: ./login/');
+die();
+?>
