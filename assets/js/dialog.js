6fb610d530b9656fcf66c69523fcca44f98e2b27
David Saah <dasorange.hope@gmail.com>
2024-02-25 22:27:58 +0000

    feat: serve multiple edit dialog boxes

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -6,3 +6,19 @@ document.getElementById("openDialog").addEventListener("click", function () {
 document.getElementById("closeDialog").addEventListener("click", function () {
   document.getElementById("dialog").close();
 });
+
+// create handler for multiple edit dialogs
+document.querySelectorAll(".table-row-group").forEach((row) => {
+  const cid = row.querySelector(".table-cell").textContent;
+  const editButton = document.getElementById(`edit-${cid}`);
+  const dialog = document.getElementById(`dialog-${cid}`);
+  const closeButton = document.getElementById(`close-${cid}`);
+
+  editButton.addEventListener("click", () => {
+    dialog.classList.remove("hidden");
+  });
+
+  closeButton.addEventListener("click", () => {
+    dialog.classList.add("hidden");
+  });
+});
