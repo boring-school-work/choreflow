624f70aa4dab315dba0b4d4af971c1ac32ed1266
David Saah <dasorange.hope@gmail.com>
2024-02-28 00:14:56 +0000

    fix: update query to show only unassigned chores

--- functions/select_chore.php
+++ functions/select_chore.php
@@ -2,7 +2,7 @@
 
 function get_chore($conn)
 {
-  $sql = "SELECT * FROM Chores";
+  $sql = "SELECT * FROM Chores WHERE cid NOT IN (SELECT cid FROM Assignment)";
   $result = $conn->query($sql);
   foreach ($result as $row) {
     echo "<option value='" . $row['cid'] . "'>" . $row['chorename'] . "</option>";
