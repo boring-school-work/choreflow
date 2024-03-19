1a01f1657f2ae7a8dccb9b8c3f9368bc76531c41
David Saah <dasorange.hope@gmail.com>
2024-03-19 05:31:23 +0000

    feat: create get_fullname function

--- functions/get_completed_chores_list.php
+++ functions/get_completed_chores_list.php
@@ -2,6 +2,20 @@
 
 function get_completed_chores($conn)
 {
+  include "../../../functions/get_fullname.php";
+
+  echo "
+    <div class='table-header-group'>
+      <div class='table-row'>
+        <div class='table-cell bg-gray-300 py-2 pl-3'>Chore name</div>
+        <div class='table-cell bg-gray-300 py-2 pl-3'>Assigned by</div>
+        <div class='table-cell bg-gray-300 py-2 pl-3'>Date Assigned</div>
+        <div class='table-cell bg-gray-300 py-2 pl-3'>Date Completed</div>
+        <div class='table-cell bg-gray-300 py-2 pl-3'>Assignee</div>
+      </div>
+    </div>
+";
+
   $sql = "
   SELECT * FROM 
   (SELECT chorename, who_assigned, date_assign, date_completed, assignmentid 
@@ -20,7 +34,7 @@ function get_completed_chores($conn)
 
   foreach ($result as $row) {
     $chorename = $row['chorename'];
-    $who_assigned = $row['who_assigned'];
+    $who_assigned = get_fullname($conn, $row['who_assigned']);
     $date_assign = $row['date_assign'];
     $date_completed = $row['date_completed'];
     $assignee = $row['assignee'];
