978a20d1fcffd90db22cf7eefce050e97961757f
David Saah <dasorange.hope@gmail.com>
2024-03-29 17:19:21 +0000

    feat: add url parameters to show different chore status

--- view/user/dashboard/index.php
+++ view/user/dashboard/index.php
@@ -45,33 +45,70 @@ if ($_SESSION['role_id'] < 3) {
       <main class="bg-gray-100 h-[89vh] px-8">
         <h2 class="py-10 font-semibold text-gray-900 text-3xl">Tasks</h2>
         <div class="flex justify-around">
-          <a href="../manage-chores/" class="px-5 gap-y-8 pt-5 pb-3 rounded-lg drop-shadow-md bg-white w-1/4 flex flex-col justify-between">
+          <a href="./?status=inprogress" class="px-5 gap-y-8 pt-5 pb-3 rounded-lg drop-shadow-md bg-white w-1/4 flex flex-col justify-between">
             <div class="flex items-center">
               <div class="w-[12px] h-[12px] rounded-xl bg-yellow-400 mr-3"></div>
               In Progress
             </div>
 
-            <div class="font-bold text-2xl">12</div>
+            <div class="font-bold text-2xl">
+              <?php
+              include './../../../settings/connection.php';
+              include './../../../functions/user_in_prog_chores_count.php';
+              get_in_prog_count($conn);
+              $conn->close();
+              ?>
+            </div>
           </a>
-          <a href="../manage-chores/" class="px-5 gap-y-8 pt-5 pb-3 rounded-lg drop-shadow-md bg-white w-1/4 flex flex-col justify-between">
+          <a href="./?status=incomplete" class="px-5 gap-y-8 pt-5 pb-3 rounded-lg drop-shadow-md bg-white w-1/4 flex flex-col justify-between">
             <div class="flex items-center">
               <div class="w-[12px] h-[12px] rounded-xl bg-red-600 mr-3"></div>
               Incomplete
             </div>
-            <div class="font-bold text-2xl">12</div>
+            <div class="font-bold text-2xl">
+              <?php
+              include './../../../settings/connection.php';
+              include './../../../functions/user_incomplete_chores_count.php';
+              get_incomplete_count($conn);
+              $conn->close();
+              ?>
+            </div>
           </a>
-          <a href="../manage-chores/" class="px-5 gap-y-8 pt-5 pb-3 rounded-lg drop-shadow-md bg-white w-1/4 flex flex-col justify-between">
+          <a href="./?status=completed" class="px-5 gap-y-8 pt-5 pb-3 rounded-lg drop-shadow-md bg-white w-1/4 flex flex-col justify-between">
             <div class="flex items-center">
               <div class="w-[12px] h-[12px] rounded-xl bg-green-600 mr-3"></div>
               Completed
             </div>
-            <div class="font-bold text-2xl">12</div>
+            <div class="font-bold text-2xl">
+              <?php
+              include './../../../settings/connection.php';
+              include './../../../functions/user_complete_chores_count.php';
+              get_complete_count($conn);
+              $conn->close();
+              ?>
+            </div>
           </a>
         </div>
 
         <!-- Assigned Tasks -->
         <h2 class="py-10 font-semibold text-gray-900 text-3xl">
-          Assigned Tasks
+          <?php
+          if (isset($_GET['status'])) {
+            switch ($_GET['status']) {
+              case "inprogress":
+                echo "Tasks In Progress";
+                break;
+              case "completed":
+                echo "Completed Tasks ";
+                break;
+              case "incomplete":
+                echo "Incompleted Tasks ";
+                break;
+            }
+          } else {
+            echo "Assigned Tasks";
+          }
+          ?>
         </h2>
         <div class="flex flex-col gap-y-3">
           <div class="flex justify-between mx-16 px-5 border-2 items-center rounded-md">
