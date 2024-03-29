38e741eda427f52d9d05de38d6d9e68d99509f45
David Saah <dasorange.hope@gmail.com>
2024-03-29 17:17:52 +0000

    feat: get chore count for different status

---
+++ functions/user_in_prog_chores_count.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+function get_in_prog_count($conn)
+{
+  $pid = $_SESSION['user_id'];
+  $sql = "
+      SELECT COUNT(*) AS value
+      FROM Assignment
+      INNER JOIN Assigned_people
+      ON Assignment.assignmentid=Assigned_people.assignmentid
+      WHERE Assigned_people.pid=$pid AND Assignment.sid=2";
+  $result = $conn->query($sql)->fetch_assoc();
+
+  echo $result['value'];
+}
